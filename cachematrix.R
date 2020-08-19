29c19b0e08acde5cbab13dd9336a0314a4e17e8d
arunchakor <69910189+arunchakor@users.noreply.github.com>
2020-08-19 15:17:02 +0000

    Updated assignment script for Lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two function written 
+## 1. makeCacheMatrix - Creates a list that contains four functions - get, set, getMatInverse and setMatInverse
+## 2. cacheSolve - Computes Inverse of the matrix created with above function. However, before it would check in cache if
+##                 inverse is already available. Only if inverse is missing in cache, it would do the computation.
 
-## Write a short comment describing this function
+## Creates a list that contains four functions - get, set, getMatInverse and setMatInverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
 
+        get <- function() x
+        setMatInverse <- function(inverse) i <<- inverse
+        getMatInverse <- function() i
+        list(set = set,
+             get = get,
+             setMatInverse = setMatInverse,
+             getMatInverse = getMatInverse)
 }
 
 
-## Write a short comment describing this function
+## Computes inverse of a matrix if not available in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getMatInverse()
+        if (!is.null(i)) {
+                message("Retrieving cached matrix inverse")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setMatInverse(i)
+        i
 }
